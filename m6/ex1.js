016f57fa40e8bb19b628c66bb38db25012a86ede
giovannaaranntes <giovannaarantes015@gmail.com>
2025-11-12 09:14:10 -0300

    Create ex1.js

---
+++ m6/ex1.js
@@ -0,0 +1,3 @@
+// 1. Erro: problema que impede o programa de continuar sua execução corretamente.
+// 2. Exceção: é o evento gerado quando ocorre um erro que pode ser "capturado" e tratado.
+// 3. Diferença: "erro" é um problema lógico ou sintático; já "exceção" é um erro que o JS reconhece em tempo de execução e pode ser tratado com try/catch.
